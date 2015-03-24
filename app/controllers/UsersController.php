a9122e550762fed9685c2d3553d680a985e2617e
bastienlemaitre <bastien.lemaitre@live.fr>
2015-03-24 16:46:23 +0100

    REST SERVER 2ND COMMIT

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -26,12 +26,15 @@ class UsersController extends Controller {
 		$user=new User();
 		$user->setMail($this->request->getPost("mail"));
 		$user->setPassword($this->request->getPost("password"));
+		$user->setNom($this->request->getPost("nom"));
+		$user->setPrenom($this->request->getPost("prenom"));
+		$user->setRang(2);
 		if($user->save()){
 			$token=bin2hex(openssl_random_pseudo_bytes(16));
 			$this->persistent->token = $token;
-			echo '{token: "'.$token.'",inserted: true}';
+			echo '{"token":"'.$token.'","inserted":true}';
 		}else{
-			echo "{inserted:false}";
+			echo '{"inserted":false}';
 		}
 	}
 	public function checkUserExistsAction($mail){
@@ -41,26 +44,26 @@ class UsersController extends Controller {
 				"bind"       => array(1 => $mail)
 		));
 		if($user){
-			echo '{exists: true,mail: "'.$mail.'"}';
+			echo '{"exists":true,"mail":"'.$mail.'"}';
 		}else{
-			echo '{exists: false,mail: "'.$mail.'"}';
+			echo '{"exists":false,"mail":"'.$mail.'"}';
 		}
 	}
 	public function checkConnectionAction($mail,$password){
 		$this->response->setContentType('application/json', 'utf-8');
 		if($this->_auth($mail, $password)){
-			echo '{token : '.$this->token.',connected: true}';
+			echo '{"token":"'.$this->token.'","connected":true}';
 		}else{
-			echo '{connected: false}';
+			echo '{"connected":false}';
 		}
 	}
 
 	public function checkConnectedAction(){
 		$this->response->setContentType('application/json', 'utf-8');
 		if($this->session->has("token")){
-			echo '{token : '.$this->session->get("token").',connected: true}';
-		}else{
-			echo '{connected: false}';
+			echo '{"token":"'.$this->session->get("token").'","connected":true}';
+		}else{			
+			echo '{"connected":false}';
 		}
 	}
 
@@ -68,15 +71,40 @@ class UsersController extends Controller {
 		$mail=$this->request->getPost("mail");
 		$password=$this->request->getPost("password");
 		if($this->_auth($mail, $password)){
-			echo '{token : '.$this->token.',connected: true}';
+			echo '{"token":"'.$this->token.'","connected":true}';
 		}else{
-			echo '{connected: false}';
+			echo '{"connected":false}';
 		}
 	}
 
 	public function disconnectAction(){
 		$this->response->setContentType('application/json', 'utf-8');
 		$this->session->destroy();
-		echo '{connected: false}';
+		echo '{"connected":false}';
 	}
+	
+	/*
+	protected function _isValidToken($token,$force=false){
+		return $force=="true" || $this->session->get("token")===$token;
+	}
+	
+	protected function sendMessage($type,$content){
+		return array("type"=>$type,"content"=>$content);
+	}
+	
+	protected function infoMessage($message){
+		return $this->sendMessage("info",$message);
+	}
+	
+	protected function successMessage($message){
+		return $this->sendMessage("success",$message);
+	}
+	
+	protected function warningMessage($message){
+		return $this->sendMessage("warning",$message);
+	}
+	
+	protected function dangerMessage($message){
+		return $this->sendMessage("danger",$message);
+	}*/
 }
